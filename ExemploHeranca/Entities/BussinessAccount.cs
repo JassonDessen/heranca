f707b1a78e228bf04e4d098fc9bef323c9c7e284
Davi Francisco <davi-francisco214@outlook.com>
2020-06-16 20:51:28 -0300

    Sem a herança

--- ExemploHeranca/Entities/BussinessAccount.cs
+++ ExemploHeranca/Entities/BussinessAccount.cs
@@ -4,5 +4,27 @@ namespace ExemploHeranca.Entities
 {
     class BussinessAccount
     {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double LoanLimit { get; set; }
+
+        public BussinessAccount(int number, string holder, double balance, double loanLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            LoanLimit = loanLimit;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
     }
 }
